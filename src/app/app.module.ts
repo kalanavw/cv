c18aecbe3567dfaedf31577f99e0f4760dc6db6f
kalanavw <kalanaweerarathna@hotmail.com>
2023-06-09 14:14:55 +0800

    add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import {environment} from "../environments/environment";
+
 
 @NgModule({
   declarations: [
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCywHeTWTWnH-8Bot_H1Pf4JbUkBnLukuU",
+    authDomain: "webtool-e81ee.firebaseapp.com",
+    projectId: "webtool-e81ee",
+    storageBucket: "webtool-e81ee.appspot.com",
+    messagingSenderId: "803607762066",
+    appId: "1:803607762066:web:a17967256c9bafd22a824b",
+    measurementId: "G-8YQ99YZL40"
+  }
+};
