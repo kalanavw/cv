d65f9656745f2383b9ca4081cec417f7c45fe147
kalanavw <kalanaweerarathna@hotmail.com>
2023-06-18 02:07:08 +0800

    -download resume pdf

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {AppComponent} from "./app.component";
+import {KalanavwComponent} from "./components/kalanavw/kalanavw.component";
 
 const routes: Routes = [
   {
     path: '',
-    component: AppComponent
+    component: KalanavwComponent
   }
 ];
 
--- src/app/components/kalanavw/resume-header/resume-header.component.ts
+++ src/app/components/kalanavw/resume-header/resume-header.component.ts
@@ -2,6 +2,11 @@ import {Component, OnInit} from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {ResumeHeader} from "../../../dto/resume-header";
 
+export interface ResumePdf {
+  name: string;
+  pdf: string;
+}
+
 @Component({
   selector: 'app-resume-header',
   templateUrl: './resume-header.component.html',
@@ -13,12 +18,8 @@ export class ResumeHeaderComponent implements OnInit {
 
   constructor(private http: HttpClient) {
   }
-  ngOnInit(): void {
-    const httpOptions = {
-      headers: new HttpHeaders(),
-      withCredentials: true
-    };
 
+  ngOnInit(): void {
     this.http.get<ResumeHeader>('assets/data/resume_header.json', {})
       .subscribe((value: ResumeHeader) => {
         this.header = value;
@@ -26,4 +27,29 @@ export class ResumeHeaderComponent implements OnInit {
 
   }
 
+  downloadPdf() {
+    this.http.get<ResumePdf>('assets/data/resume.json', {})
+      .subscribe((value: ResumePdf) => {
+        const byteCharacters = atob(value.pdf);
+        const byteArrays = [];
+
+        for (let offset = 0; offset < byteCharacters.length; offset += 512) {
+          const slice = byteCharacters.slice(offset, offset + 512);
+          const byteNumbers = new Array(slice.length);
+          for (let i = 0; i < slice.length; i++) {
+            byteNumbers[i] = slice.charCodeAt(i);
+          }
+          const byteArray = new Uint8Array(byteNumbers);
+          byteArrays.push(byteArray);
+        }
+
+        const blob = new Blob(byteArrays, { type: 'application/pdf' });
+        const url = window.URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = value.name;
+        link.click();
+        window.URL.revokeObjectURL(url);
+      });
+  }
 }
