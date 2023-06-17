383444d154b3e54551981cb872fe5d630ffbbdb5
kalanavw <kalanaweerarathna@hotmail.com>
2023-06-18 02:54:24 +0800

    -add google analytics event

--- src/app/components/kalanavw/resume-header/resume-header.component.ts
+++ src/app/components/kalanavw/resume-header/resume-header.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {ResumeHeader} from "../../../dto/resume-header";
+import {AngularFireAnalytics} from "@angular/fire/compat/analytics";
 
 export interface ResumePdf {
   name: string;
@@ -16,7 +17,7 @@ export class ResumeHeaderComponent implements OnInit {
 
   header: ResumeHeader | undefined;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private analytics: AngularFireAnalytics) {
   }
 
   ngOnInit(): void {
@@ -28,28 +29,30 @@ export class ResumeHeaderComponent implements OnInit {
   }
 
   downloadPdf() {
-    this.http.get<ResumePdf>('assets/data/resume.json', {})
-      .subscribe((value: ResumePdf) => {
-        const byteCharacters = atob(value.pdf);
-        const byteArrays = [];
+    this.analytics.logEvent('resume_download', {"date_time": new Date}).then(r => {
+      this.http.get<ResumePdf>('assets/data/resume.json', {})
+        .subscribe((value: ResumePdf) => {
+          const byteCharacters = atob(value.pdf);
+          const byteArrays = [];
 
-        for (let offset = 0; offset < byteCharacters.length; offset += 512) {
-          const slice = byteCharacters.slice(offset, offset + 512);
-          const byteNumbers = new Array(slice.length);
-          for (let i = 0; i < slice.length; i++) {
-            byteNumbers[i] = slice.charCodeAt(i);
+          for (let offset = 0; offset < byteCharacters.length; offset += 512) {
+            const slice = byteCharacters.slice(offset, offset + 512);
+            const byteNumbers = new Array(slice.length);
+            for (let i = 0; i < slice.length; i++) {
+              byteNumbers[i] = slice.charCodeAt(i);
+            }
+            const byteArray = new Uint8Array(byteNumbers);
+            byteArrays.push(byteArray);
           }
-          const byteArray = new Uint8Array(byteNumbers);
-          byteArrays.push(byteArray);
-        }
 
-        const blob = new Blob(byteArrays, { type: 'application/pdf' });
-        const url = window.URL.createObjectURL(blob);
-        const link = document.createElement('a');
-        link.href = url;
-        link.download = value.name;
-        link.click();
-        window.URL.revokeObjectURL(url);
-      });
+          const blob = new Blob(byteArrays, { type: 'application/pdf' });
+          const url = window.URL.createObjectURL(blob);
+          const link = document.createElement('a');
+          link.href = url;
+          link.download = value.name;
+          link.click();
+          window.URL.revokeObjectURL(url);
+        });
+    });
   }
 }
